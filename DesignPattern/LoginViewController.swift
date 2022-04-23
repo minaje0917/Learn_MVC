902172918a7ea90c4476e018480e4b2b36305909
minaje0917 <k050918@naver.com>
2022-04-23 20:22:29 +0900

    day 1

--- DesignPattern/LoginViewController.swift
+++ DesignPattern/LoginViewController.swift
@@ -11,11 +11,17 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        emailField.addTarget(self, action: #selector(self.validateFields), for: .editingChanged)
+        passwordField.addTarget(self, action: #selector(self.validateFields), for: .editingChanged)
+    }
+    
+    @objc private func validateFields() {
+        loginButton.isEnabled = emailField.text != "" && passwordField.text != ""
     }
     
     @IBAction func loginBtnClicked(_ sender: UIButton) {
---
+++ User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  DesignPattern
+//
+//  Created by 선민재 on 2022/04/23.
+//
+
+import Foundation
+
+
+struct User {
+    let firstName, lastName, email : String
+    let age: Int
+    let location: Location
+}
+
+struct Location {
+    let lat: Double
+    let lng: Double
+}
